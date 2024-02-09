960956be47e219a547eb25ce4e7c79283cee5213
patrickverroya <21-05720@g.batstate-u.edu.ph>
2024-02-09 19:29:33 +0800

    update

--- SCARA_PRR.m
+++ SCARA_PRR.m
@@ -7,7 +7,7 @@ a1 = 5;
 a2 = 3;
 a3 = 3;
 a4 = 4;
-
+d1 = 5
 
 %% D-H Parameters: theta, d, r, alpha, offset
 % if prismatic joint: theta = theta, d = 0, offset = 1, after offset put the value of d
@@ -15,7 +15,7 @@ a4 = 4;
 
 
 H0_1 = Link([0,0,a2,0,1,a1]); 
-H0_1.qlim = [0 5];
+H0_1.qlim = [0 d1];
 
 H1_2 = Link([0,0,a3,0,0,0]); 
 H1_2.qlim = pi/180*[-90 90];
