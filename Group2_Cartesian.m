e48e60982fdfccc573aaf48e49a41c23cdefdb8a
patrickverroya <21-05720@g.batstate-u.edu.ph>
2024-02-12 07:49:10 +0800

    Update Group2_Cartesian.m

--- Group2_Cartesian.m
+++ Group2_Cartesian.m
@@ -7,18 +7,15 @@ a1 = 5;
 a2 = 3;
 a3 = 3;
 a4 = 2;
-
 %% Joint Variables
 d1 = 2;
 d2 = 2;
 d3 = 2;
-
-%% D-H Parameters [theta; d; r; alpha; offset]
-% if prismatic joint: theta = theta, d = 0, offset = 1, after put the value of d
-% if revolute joint: theta = 0, offsett = 0, after put the value of theta
-
+%% D-H Parameters [theta, d, r, alpha, offset]
+% if prismatic joint: theta = theta, d = 0, offset = 1, after offset put the value of d
+% if revolute joint: theta = 0,offset = 0, after offset put the value of theta
 H0_1 = Link([0,0,0,pi/2,1,-a1]);
-H_01.qlim = [0 0];
+H0_1.qlim = [0 0];
 
 H1_2 = Link([pi/2,0,0,pi/2,1,a2]);
 H1_2.qlim = [0 d1];
@@ -30,6 +27,6 @@ H3_4 = Link([0,0,0,0,1,a4]);
 H3_4.qlim = [0 d3];
 
 
-Cart = SerialLink([H0_1 H1_2 H2_3 H3_4], 'name','Cart')
-Cart.plot([0 0 0 0],'workspace',[-2 8 -10 2 -10 0]) % plot at Origin position
+Cart = SerialLink([H0_1 H1_2 H2_3 H3_4], 'name', 'Cart')
+Cart.plot([0 0 0 0],'workspace',[-2 8 -10 2 -10 0]) %plot at Origin position
 Cart.teach
