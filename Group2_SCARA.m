60b45ab2e89b4bd358a6c31a06fb3a64350194f0
jhannrey <158046427+jhannrey@users.noreply.github.com>
2024-02-12 07:36:23 +0800

    Update Group2_SCARA.m

--- Group2_SCARA.m
+++ Group2_SCARA.m
@@ -10,7 +10,6 @@ a4 = 4;
 d1 = 5
 
 %% D-H Parameters: theta, d, r, alpha, offset
-
 % if prismatic joint: theta = theta, d = 0, offset = 1, after offset put the value of d
 % if revolute joint: theta = 0,offset = 0, after offset put the value of theta
 
